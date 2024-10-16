f36b41fa627c954bfb489782e37c3040791024e3
Elena Pashkova <pashkova.e19@gmail.com>
2024-10-16 17:51:05 -0400

    Revert "Login added"

--- utils/api.ts
+++
@@ -1,9 +0,0 @@
-// api.ts
-import axios, { AxiosInstance } from 'axios';
-
-const api: AxiosInstance = axios.create({
-  baseURL: 'https://api.coingecko.com/api/v3',
-  timeout: 10000,
-});
-
-export default api;
